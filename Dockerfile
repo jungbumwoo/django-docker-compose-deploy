8bc51cb89809fee4ed954f188eaee5093b3b80f4
Jungbum Woo <jungbumwoo@naver.com>
2022-03-30 22:34:56 +0900

    add postgresql

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,11 @@ EXPOSE 8000
 
 RUN python -m venv /py && \
     /py/bin/pip install --upgrade pip && \
+    apk add --update --no-cache postgresql-client && \
+    apk add --update --no-cache --virtual .tmp-deps \
+      build-base postgresql-dev musl-dev && \
     /py/bin/pip install -r /requirements.txt && \
+    apk del .tmp-deps && \
     adduser --disabled-password --no-create-home app # 한 줄로 실행하는게 좋음 도커는 명령마다 새로운 이미지 레이어를 만들기에 이미지를 줄이기 위해선 한 줄에 쭉
 # 명령들이 실행되면 여기서 실행됨
 ENV PATH="/py/bin:$PATH"
--- app/app/settings.py
+++ app/app/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7o8r)d)z-9cj&w_)eu%^4)h4v6i9m__5!(%gbhc9^r@*czvvkc'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# SECURITY WARNING: don't run with debug turned on in production! # backgroun 에서 돌아가는게 많기 때문
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 
 # Application definition
@@ -75,8 +81,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+
     }
 }
 
